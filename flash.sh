e1e376de55a1237b869c5d68e68f1aaa469e5974
Ryan Cole <admin@ryanc.me>
2021-12-27 18:21:26 +1300

    Increase baud in flash.sh

--- flash.sh
+++ flash.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-powershell.exe -Command "espflash --monitor COM5 ./target/xtensa-esp32s2-espidf/debug/main"
+powershell.exe -Command "espflash --speed 460800 COM5 ./target/xtensa-esp32s2-espidf/debug/main; espmonitor COM5"
