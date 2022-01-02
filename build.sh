fc83ce9ae5d02c4c96832f5cc28b8064640d33cc
Ryan Cole <admin@ryanc.me>
2022-01-02 15:13:20 +1300

    Remove old build/flash scripts

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "--- building 'ulp' ---"
-cd ulp
-cargo build
-
-echo "--- building 'main' ---"
-cd ../main
-cargo build --features native
--- flash.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-powershell.exe -Command "espflash --speed 460800 COM5 ./target/xtensa-esp32s2-espidf/debug/main; espmonitor COM5"
